326c8560c6e8fbc7edfe12c24a8c2e7e8ef0acee
Ethan Senor <49727841+erssenor@users.noreply.github.com>
2019-12-03 13:11:29 -0500

    Delete GlobalVar.h

--- GlobalVar.h
+++
@@ -1,6 +0,0 @@
-//Created by Ethan Senor
-#pragma once
-//This contains the global variables used for the game. The only neccessay variables are the dimensions
-const int MAP_HEIGHT = 25;
-const int MAP_WIDTH = 25;
-
